81ccf54751f20976cd160daba28f57d51864112e
mauriciobrito7 <maubot01@gmail.com>
2020-04-03 20:14:25 -0400

    transiciones

---
+++ 04_transiciones/index.js
@@ -0,0 +1,16 @@
+let mybtn = document.getElementById("animate");
+
+const animate = event => {
+    document.body.classList.add("animate");
+    /*Las animaciones tienen un evento llamado transitionend 
+                                      es un unico evento
+                                      */
+    document.getElementById("ball").addEventListener("transitionend", e => {
+        console.log(e);
+        if (e.elapsedTime === 1) {
+            document.body.classList.remove("animate");
+        }
+    });
+};
+
+mybtn.addEventListener("click", () => animate(event));
