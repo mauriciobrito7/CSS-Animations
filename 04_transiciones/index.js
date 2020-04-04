1e650e6dbcdfe490a1d5129cdb2322b970f82aec
mauriciobrito7 <maubot01@gmail.com>
2020-04-03 22:37:19 -0400

    triggers de transiciones

--- 04_transiciones/index.js
+++ 04_transiciones/index.js
@@ -3,8 +3,8 @@ let mybtn = document.getElementById("animate");
 const animate = event => {
     document.body.classList.add("animate");
     /*Las animaciones tienen un evento llamado transitionend 
-                                              es un unico evento
-                                              */
+                                                es un unico evento
+                                                */
     document.getElementById("ball").addEventListener("transitionend", e => {
         console.log(e);
         if (e.elapsedTime === 3) {
