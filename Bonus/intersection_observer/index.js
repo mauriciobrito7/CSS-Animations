ba06ed66dd4748868fa938f6e7952f4e5cac8b3e
mauriciobrito7 <maubot01@gmail.com>
2020-04-08 17:35:17 -0400

    fix error transitions

--- Bonus/intersection_observer/index.js
+++ Bonus/intersection_observer/index.js
@@ -47,22 +47,28 @@ function preloadImage(img) {
 
 // Observer images
 
-const observerImg = new IntersectionObserver((entries, self) => {
-    // iterate over each entry
-    entries.forEach((entry) => {
-        // process just the images that are intersecting.
-        // isIntersecting is a property exposed by the interface
-        if (entry.isIntersecting) {
-            // custom function that copies the path to the img
-            // from data-src to src
-            preloadImage(entry.target);
-            // the image is now in place, stop watching
-            self.unobserve(entry.target);
-        }
-    });
-}, configImage);
+const targets = document.querySelectorAll("img");
 
-const imgs = document.querySelectorAll("[data-src]");
-imgs.forEach((img) => {
-    observerImg.observe(img);
-});
+const lazyLoad = (target) => {
+    const io = new IntersectionObserver((entries, observer) => {
+        console.log(entries);
+        entries.forEach((entry) => {
+            console.log("😍");
+            // process just the images that are intersecting.
+            // isIntersecting is a property exposed by the interface
+            if (entry.isIntersecting) {
+                const img = entry.target;
+                const src = img.getAttribute("data-src");
+
+                img.setAttribute("src", src);
+                img.classList.add("fade");
+                // the image is now in place, stop watching
+                observer.disconnect();
+            }
+        });
+    });
+
+    io.observe(target);
+};
+
+targets.forEach(lazyLoad);
